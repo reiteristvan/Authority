fdcdeeb337c745084f52749608d19da491f2f2a8
Istvan Reiter <reiteristvan@users.noreply.github.com>
2015-10-17 12:01:15 +0000

    IdentityServer.Web seed checkin

---
+++ IdentityServer/IdentityServer.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace IdentityServer.Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+        }
+    }
+}
