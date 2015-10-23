6df0fa09cb541be83385036ebed6059ee13062bb
Istvan Reiter <reiteristvan@users.noreply.github.com>
2015-10-23 12:59:21 +0000

    Added base repository class and set up EF

---
+++ IdentityServer/IdentityServer.EntityFramework/Configurations/ClientApplicationConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using IdentityServer.DomainModel;
+
+namespace IdentityServer.EntityFramework.Configurations
+{
+    public sealed class ClientApplicationConfiguration : EntityTypeConfiguration<ClientApplication>
+    {
+        public ClientApplicationConfiguration()
+        {
+            ToTable("ClientApplications");
+
+            HasKey(e => e.Id);
+
+            Property(c => c.Name).HasMaxLength(128);
+            Property(c => c.ApplicationId).HasMaxLength(128);
+            Property(c => c.ApplicationSecret).HasMaxLength(128);
+            Property(c => c.RedirectUrl).HasMaxLength(128);
+            Property(c => c.IsEnabled);
+            
+            HasRequired(c => c.Product);
+        }
+    }
+}
