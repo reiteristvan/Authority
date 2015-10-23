edf46341e46517828516cc00326af5d3fce0723f
Istvan Reiter <istvanreiter@outlook.com>
2015-10-23 09:53:05 +0000

    Some fixes, added oauth controller

---
+++ IdentityServer/IdentityServer.Web/Controllers/OAuthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityServer.Web.Controllers
+{
+    [RoutePrefix("oauth")]
+    public class OAuthController : Controller
+    {
+        [Route]
+        public ActionResult Index(string response_type, string client_id, string redirect_uri, string scope, string state = "")
+        {
+            return View();
+        }
+    }
+}
--- IdentityServer/IdentityServer.Web/Global.asax.cs
+++ IdentityServer/IdentityServer.Web/Global.asax.cs
@@ -8,8 +8,8 @@ namespace IdentityServer.Web
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
