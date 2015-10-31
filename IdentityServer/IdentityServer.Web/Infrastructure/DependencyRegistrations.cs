9b1deaf42cc855d4d56c43bceb9f2e3d765af524
Istvan Reiter <reiteristvan@users.noreply.github.com>
2015-10-31 14:11:38 +0000

    Setting up DI

---
+++ IdentityServer/IdentityServer.Web/Infrastructure/DependencyRegistrations.cs
@@ -0,0 +1,17 @@
+﻿using IdentityServer.EntityFramework;
+using Microsoft.Practices.Unity;
+
+namespace IdentityServer.Web.Infrastructure
+{
+    public static class DependencyRegistrations
+    {
+        public static IUnityContainer Register()
+        {
+            UnityContainer container = new UnityContainer();
+
+            container.RegisterType<IIdentityServerContext, IdentityServerContext>(new ContainerControlledLifetimeManager());
+
+            return container;
+        }
+    }
+}
