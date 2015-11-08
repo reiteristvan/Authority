25f97bff4d8f86599f6a652aa3f542f267339698
Istvan Reiter <istvanreiter@outlook.com>
2015-11-08 10:51:17 +0000

    Added Email service

---
+++ IdentityServer/IdentityServer.Web/Infrastructure/EmailSettingsProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+using IdentityServer.EmailService.cs;
+
+namespace IdentityServer.Web.Infrastructure
+{
+    public sealed class EmailSettingsProvider : IEmailSettingsProvider
+    {
+        public string TemplateFolderPath { get { return ConfigurationManager.AppSettings["EmailTemplatePath"]; } }
+    }
+}
