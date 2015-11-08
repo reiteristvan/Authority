344b1640c7c882ef3d519d0a5d0b47e81cc2810c
Istvan Reiter <reiteristvan@users.noreply.github.com>
2015-11-08 12:49:51 +0000

    Added test project

---
+++ IdentityServer/IdentityServer.Tests/EmailService/Models/TestActivation.cs
@@ -0,0 +1,10 @@
+﻿using IdentityServer.EmailService.cs;
+
+namespace IdentityServer.Tests.EmailService.Models
+{
+    [EmailTemplateName("TestActivation")]
+    public class TestActivation
+    {
+        public string Email { get; set; }
+    }
+}
---
+++ IdentityServer/IdentityServer.Tests/EmailService/TemplateProviderTests.cs
@@ -0,0 +1,23 @@
+﻿using IdentityServer.EmailService.cs;
+using IdentityServer.Tests.EmailService.Models;
+using Xunit;
+
+namespace IdentityServer.Tests.EmailService
+{
+    public sealed class TemplateProviderTests
+    {
+        [Fact]
+        public void Positive()
+        {
+            EmailTemplateProvider templateProvider = new EmailTemplateProvider(new TestEmailSettingsProvider());
+            TestActivation model = new TestActivation
+            {
+                Email = "reiteristvan@gmail.com"
+            };
+
+            string result = templateProvider.Load(model);
+
+            Assert.NotNull(result);
+        }
+    }
+}
