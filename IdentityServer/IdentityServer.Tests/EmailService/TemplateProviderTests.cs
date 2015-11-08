f52df243b29abf4a167b3b903b3668ffbbd14ca8
Istvan Reiter <reiteristvan@users.noreply.github.com>
2015-11-08 15:24:15 +0000

    Fixed email template loader + Added fixtiure to test

--- IdentityServer/IdentityServer.Tests/EmailService/TemplateProviderTests.cs
+++ IdentityServer/IdentityServer.Tests/EmailService/TemplateProviderTests.cs
@@ -1,22 +1,45 @@
-﻿using IdentityServer.EmailService.cs;
+﻿using System;
+using IdentityServer.EmailService.cs;
 using IdentityServer.Tests.EmailService.Models;
 using Xunit;
 
 namespace IdentityServer.Tests.EmailService
 {
-    public sealed class TemplateProviderTests
+    public sealed class TemplateTestFixture : IDisposable
     {
+        private readonly EmailTemplateProvider _templateProvider;
+
+        public TemplateTestFixture()
+        {
+            _templateProvider = new EmailTemplateProvider(new TestEmailSettingsProvider());
+        }
+
+        public EmailTemplateProvider TemplateProvider { get { return _templateProvider; } }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    public sealed class TemplateProviderTests : IClassFixture<TemplateTestFixture>
+    {
+        private readonly TemplateTestFixture _testFixture;
+
+        public TemplateProviderTests(TemplateTestFixture testFixture)
+        {
+            _testFixture = testFixture;
+        }
+
         [Fact]
         public void Positive()
         {
-            EmailTemplateProvider templateProvider = new EmailTemplateProvider(new TestEmailSettingsProvider());
             TestActivation model = new TestActivation
             {
                 Email = "reiteristvan@gmail.com"
             };
 
-            string result = templateProvider.Load(model);
-
+            string result = _testFixture.TemplateProvider.Load(model);
+           
             Assert.NotNull(result);
         }
     }
