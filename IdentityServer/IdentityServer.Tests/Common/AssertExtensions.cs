8f70157f08eb593a1c9f7eff4463b9fdff9bd107
Istvan Reiter <istvanreiter@outlook.com>
2015-12-30 17:47:09 +0000

    Extended test framework

---
+++ IdentityServer/IdentityServer.Tests/Common/AssertExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IdentityServer.Tests.Common
+{
+    public static class AssertExtensions
+    {
+        public static async Task ThrowAsync<TException>(Func<Task> task)
+        {
+            var expected = typeof (TException);
+            Type actual = null;
+
+            try
+            {
+                await task();
+            }
+            catch (Exception e)
+            {
+                actual = e.GetType();
+            }
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
