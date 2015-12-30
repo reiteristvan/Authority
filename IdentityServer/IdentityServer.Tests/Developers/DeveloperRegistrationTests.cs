9f7ba31d1aeb554b297c82861f0ee0b42fbb0e4f
Istvan Reiter <istvanreiter@outlook.com>
2015-12-30 17:47:32 +0000

    --

--- IdentityServer/IdentityServer.Tests/Developers/DeveloperRegistrationTests.cs
+++ IdentityServer/IdentityServer.Tests/Developers/DeveloperRegistrationTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using IdentityServer.DomainModel;
 using IdentityServer.EntityFramework;
