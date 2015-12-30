a862aebda098643805b003cc41ea235f0ac88500
Istvan Reiter <istvanreiter@outlook.com>
2015-12-30 13:40:57 +0000

    Showing error id to the user

---
+++ IdentityServer/IdentityServer.Web/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace IdentityServer.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Show(Guid id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
