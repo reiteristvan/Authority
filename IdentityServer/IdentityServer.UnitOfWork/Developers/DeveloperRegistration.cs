6834febde61b9a80fe8fdd7365eecbf9fe55ce0c
Istvan Reiter <reiteristvan@users.noreply.github.com>
2016-02-06 15:16:43 +0000

    Added async Operation bases + refactor developer operations to use the new operation classes

--- IdentityServer/IdentityServer.UnitOfWork/Developers/DeveloperRegistration.cs
+++ IdentityServer/IdentityServer.UnitOfWork/Developers/DeveloperRegistration.cs
@@ -8,7 +8,7 @@ using IdentityServer.UnitOfWork.Utilities;
 
 namespace IdentityServer.UnitOfWork.Developers
 {
-    public sealed class DeveloperRegistration : OperationWithReturnValue<Task<Developer>>
+    public sealed class DeveloperRegistration : OperationWithReturnValueAsync<Developer>
     {
         private readonly string _email;
         private readonly string _displayname;
@@ -23,32 +23,6 @@ namespace IdentityServer.UnitOfWork.Developers
             _password = password;
             _passwordService = new PasswordService();
         }
-
-        //public async Task<Developer> Register(string email, string displayname, string password)
-        //{
-        //    await Check(() => IsUserNotExist(email), DevelopersErrorCodes.EmailAlreadyExists);
-        //    await Check(() => IsUsernameAvailable(displayname), DevelopersErrorCodes.DisplayNameNotAvailable);
-
-        //    byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
-        //    byte[] saltBytes = _passwordService.CreateSalt();
-        //    byte[] hashBytes = _passwordService.CreateHash(passwordBytes, saltBytes);
-
-        //    Developer developer = new Developer
-        //    {
-        //        Email = email,
-        //        DisplayName = displayname,
-        //        Salt = Convert.ToBase64String(saltBytes),
-        //        PasswordHash = Convert.ToBase64String(hashBytes),
-        //        IsActive = true,
-        //        IsPending = true,
-        //        PendingRegistrationId = Guid.NewGuid(),
-        //        Created = DateTime.UtcNow
-        //    };
-
-        //    Context.Developers.Add(developer);
-
-        //    return developer;
-        //}
 
         private async Task<bool> IsUserNotExist(string email)
         {
