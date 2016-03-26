828a07a046ce212d70ebdd6692ef06d3a30fdc74
Istvan Reiter <reiteristvan@users.noreply.github.com>
2016-03-26 14:50:24 +0000

    Developers index page

---
+++ IdentityServer/IdentityServer.Web/Infrastructure/HtmlHelpers/HtmlExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace IdentityServer.Web.Infrastructure.HtmlHelpers
+{
+    public static class HtmlExtensions
+    {
+        public static MvcHtmlString RestLink(this HtmlHelper htmlHelper, string title, string format, params object[] parameters)
+        {
+            string url = string.Format(format, parameters);
+            return MvcHtmlString.Create(string.Format("<a href='{0}'>{1}</a>", url, title));
+        }
+    }
+}
