da8396e3b24e746fcd84afebfc33485cb7ec35b0
Istvan Reiter <istvanreiter@outlook.com>
2016-04-17 07:25:52 +0000

    Big renaming fix

---
+++ Authority/Authority.Operations/Products/GetProductDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Authority.DomainModel;
+using Authority.EntityFramework;
+
+namespace Authority.Operations.Products
+{
+    public sealed class GetProductDetails : SafeOperation
+    {
+        public GetProductDetails(ISafeAuthorityContext safeAuthorityContext)
+            : base(safeAuthorityContext)
+        {
+            
+        }
+
+        public async Task<Product> GetDetails(Guid userId, Guid productId)
+        {
+            Product product = await _AuthorityContext.Products
+                .Include(p => p.Policies)
+                .Include(p => p.Policies.Select(po => po.Claims))
+                .FirstOrDefaultAsync(p => p.Id == productId);
+
+            Check(() => product.OwnerId == userId, ProductErrorCodes.UnAuthorizedAccess);
+
+            return product;
+        }
+    }
+}
