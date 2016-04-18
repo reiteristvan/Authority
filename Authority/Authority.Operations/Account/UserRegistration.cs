cfeab5b04863245057a8dd790b0859efe6191845
Istvan Reiter <reiteristvan@users.noreply.github.com>
2016-04-18 12:20:45 +0000

    User registration operation

--- Authority/Authority.Operations/Account/UserRegistration.cs
+++ Authority/Authority.Operations/Account/UserRegistration.cs
@@ -10,36 +10,45 @@ namespace Authority.Operations.Account
 {
     public sealed class Registration : OperationWithReturnValueAsync<User>
     {
+        private readonly Guid _clientId;
         private readonly string _email;
         private readonly string _username;
         private readonly string _password;
         private readonly PasswordService _passwordService;
 
-        public Registration(IAuthorityContext AuthorityContext, string email, string username, string password)
+        public Registration(IAuthorityContext AuthorityContext, Guid clientId, string email, string username, string password)
             : base(AuthorityContext)
         {
+            _clientId = clientId;
             _email = email;
             _username = username;
             _password = password;
             _passwordService = new PasswordService();
         }
 
-        private async Task<bool> IsUserExist(string email)
+        private async Task<bool> IsProductAvailable()
         {
-            User user = await Context.Users.FirstOrDefaultAsync(u => u.Email == email);
+            Product product = await Context.Products.FirstOrDefaultAsync(p => p.ClientId == _clientId);
+            return product != null && product.IsPublic && product.IsActive;
+        }
+
+        private async Task<bool> IsUserExist()
+        {
+            User user = await Context.Users.FirstOrDefaultAsync(u => u.Email == _email);
             return user != null;
         }
 
-        private async Task<bool> IsUsernameAvailable(string username)
+        private async Task<bool> IsUsernameAvailable()
         {
-            User user = await Context.Users.FirstOrDefaultAsync(p => p.Username == username);
+            User user = await Context.Users.FirstOrDefaultAsync(p => p.Username == _username);
             return user != null;
         }
 
         public override async Task<User> Do()
         {
-            await Check(() => IsUserExist(_email), AccountErrorCodes.EmailAlreadyExists);
-            await Check(() => IsUsernameAvailable(_username), AccountErrorCodes.UsernameNotAvailable);
+            await Check(() => IsProductAvailable(), AccountErrorCodes.EmailAlreadyExists);
+            await Check(() => IsUserExist(), AccountErrorCodes.EmailAlreadyExists);
+            await Check(() => IsUsernameAvailable(), AccountErrorCodes.UsernameNotAvailable);
 
             byte[] passwordBytes = Encoding.UTF8.GetBytes(_password);
             byte[] saltBytes = _passwordService.CreateSalt();
