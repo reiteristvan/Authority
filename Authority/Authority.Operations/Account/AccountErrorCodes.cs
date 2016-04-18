0f7dd57243b4dbcf29e052d2762b41034f7159d2
Istvan Reiter <istvanreiter@outlook.com>
2016-04-18 12:20:45 +0000

    User registration operation

--- Authority/Authority.Operations/Account/AccountErrorCodes.cs
+++ Authority/Authority.Operations/Account/AccountErrorCodes.cs
@@ -1,13 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Authority.DomainModel;
-
-namespace Authority.Operations.Account
+﻿namespace Authority.Operations.Account
 {
     // 1XXX
     public static class AccountErrorCodes
     {
-        public static int EmailAlreadyExists = 1000;
-        public static int UsernameNotAvailable = 1001;
+        public static int ProductNotAvailable = 1000;
+        public static int EmailAlreadyExists = 1001;
+        public static int UsernameNotAvailable = 1002;
     }
 }
