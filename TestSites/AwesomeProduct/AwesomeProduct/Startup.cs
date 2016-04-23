8c8232e6bd64b0ac6a274d98e8d3d564c24e708c
Istvan Reiter <istvanreiter@outlook.com>
2016-04-23 09:33:59 +0000

    Added AwesomeProduct testsite

---
+++ TestSites/AwesomeProduct/AwesomeProduct/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AwesomeProduct.Startup))]
+namespace AwesomeProduct
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
