959e2944b6d0c1311820874943391494bd04c86b
Istvan Reiter <istvanreiter@outlook.com>
2016-04-24 09:52:16 +0000

    Some fix + added auth filter for API actions

--- Authority/Authority.Web/App_Start/WebApiConfig.cs
+++ Authority/Authority.Web/App_Start/WebApiConfig.cs
@@ -1,5 +1,11 @@
-﻿using System.Net.Http.Formatting;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Web.Http.Filters;
+using Authority.Web.Infrastructure;
+using IdentityServer.Web.Infrastructure;
+using Microsoft.Practices.Unity;
 
 namespace Authority.Web
 {
@@ -17,6 +23,15 @@ namespace Authority.Web
 
             config.Formatters.Clear();
             config.Formatters.Add(new JsonMediaTypeFormatter());
+
+            IUnityContainer container = DependencyRegistrations.Register();
+            config.DependencyResolver = new WebApiUnityDependencyResolver(container);
+
+            List<IFilterProvider> filterProviders = config.Services.GetFilterProviders().ToList();
+            IFilterProvider defaultFilterProvider = filterProviders.Single(p => p is ActionDescriptorFilterProvider);
+
+            config.Services.Remove(typeof (IFilterProvider), defaultFilterProvider);
+            config.Services.Add(typeof(IFilterProvider), new WebApiUnityFilterProvider(container));
         }
     }
 }
---
+++ Authority/Authority.Web/Infrastructure/Filters/ApiTokenFilter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using IdentityServer.Services;
+using Microsoft.Practices.Unity;
+
+namespace IdentityServer.Web.Infrastructure.Filters
+{
+    public class ApiTokenFilter : ActionFilterAttribute
+    {
+        [Dependency]
+        public IAccountService AccountService { get; set; }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            AuthenticationHeaderValue authorizationHeader = actionContext.Request.Headers.Authorization;
+
+            if (authorizationHeader == null || !authorizationHeader.Scheme.Equals("basic", StringComparison.OrdinalIgnoreCase))
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.Forbidden);
+                return;
+            }
+
+            byte[] base64credentials = Convert.FromBase64String(authorizationHeader.Parameter);
+            string credentials = Encoding.ASCII.GetString(base64credentials);
+
+            int separator = credentials.IndexOf(':');
+            string clientIdString = credentials.Substring(0, separator);
+            string clientSecretString = credentials.Substring(separator + 1);
+
+            Guid clientId = Guid.Parse(clientIdString);
+            Guid clientSecret = Guid.Parse(clientSecretString);
+
+            if(!AccountService.ValidateProductWithSecret(clientId, clientSecret).Result)
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.Forbidden);
+            }
+        }
+
+        public override async Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            AuthenticationHeaderValue authorizationHeader = actionContext.Request.Headers.Authorization;
+
+            if (authorizationHeader == null || !authorizationHeader.Scheme.Equals("basic", StringComparison.OrdinalIgnoreCase))
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.Forbidden);
+                return;
+            }
+
+            byte[] base64credentials = Convert.FromBase64String(authorizationHeader.Parameter);
+            string credentials = Encoding.ASCII.GetString(base64credentials);
+
+            int separator = credentials.IndexOf(':');
+            string clientIdString = credentials.Substring(0, separator);
+            string clientSecretString = credentials.Substring(separator + 1);
+
+            Guid clientId = Guid.Parse(clientIdString);
+            Guid clientSecret = Guid.Parse(clientSecretString);
+
+            if (!await AccountService.ValidateProductWithSecret(clientId, clientSecret))
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.Forbidden);
+            }
+        }
+    }
+}
